df6d05962e0e59eab6c123539bb4c17da428c78d
shsandro <shsandro@github.com>
2019-12-27 12:23:48 -0300

    perguntas para ansiedade funcionando

---
+++ ansiedade.pl
@@ -0,0 +1,30 @@
+:- [util].
+
+escreveResposta(0, Out) :- 
+            writeln(Out, "'not_at_all'.").
+
+escreveResposta(1, Out) :- 
+            writeln(Out, "'several_days'.").
+
+escreveResposta(2, Out) :- 
+            writeln(Out, "'more_than_half_the_days'.").
+
+escreveResposta(3, Out) :- 
+            writeln(Out, "'nearly_every_day'.").
+
+exibePerguntas([], Out).
+
+exibePerguntas([L | R], Out) :-
+            writeln(L),
+            read(Resposta),
+            escreveResposta(Resposta, Out),
+            exibePerguntas(R, Out).
+
+perguntas() :-
+            writeln('\nPara cada pergunta escolha uma das possíveis alternativas:'),
+            writeln('0 - not at all        1 - several days        2 - more than half the days          3 - nearly every day\n'),
+            writeln('Responda 0, 1, 2 ou 3 para as próximas perguntas.\n'),
+            getLines(L, 'perguntas-ansiedade.txt'),
+            open('respostas.txt', write, Out),
+            exibePerguntas(L, Out),
+            close(Out).
